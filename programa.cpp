007480312e4e8a3b9551fbb4ecebb7624ace6b98
i62gacar <43578515+i62gacar@users.noreply.github.com>
2018-12-09 20:12:23 +0000

    Modificacion roberto

--- programa.cpp
+++ programa.cpp
@@ -128,3 +128,124 @@ void Agenda::Eliminar_Alumno(){
 
 	
 }
+
+void Agenda::Modificar_Alumno(){
+	string alumno, cadena;
+	int opc=-1, n=0, i, h=0, aux;
+	bool lider;
+	int datos[12];
+	cout<<"Introduzca el DNI del alumno que desea modificar"<<endl;
+	cin>>alumno;
+	if(Buscar_Alumno(alumno)==false){
+		cout<<"El alumno no se encuentra en la agenda"<<endl;
+		return;
+	}
+	else{
+		while(Get_Alumno(h).Get_DNI()!=alumno){
+			h++;
+		}
+		cout<<"Introduzca los datos que desea modificar:"<<endl;
+		while(opc!=0){
+			cout<<"1. Nombre"<<endl;
+			cout<<"2. Apellidos"<<endl;
+			cout<<"3. DNI"<<endl;
+			cout<<"4. Email"<<endl;
+			cout<<"5. Direccion"<<endl;
+			cout<<"6. Telefono"<<endl;
+			cout<<"7. Fecha de nacimiento"<<endl;
+			cout<<"8. Curso mas alto"<<endl;
+			cout<<"9. Equipo"<<endl;
+			cout<<"10. Lider"<<endl;
+			cout<<"0. SALIR"<<endl;
+			cin>>aux;
+			if(aux>0 && aux<11){
+				datos[n]=aux;
+				n++;
+			}
+			else if(aux==0){
+
+			}
+			else{
+				cout<<"Opcion incorrecta"<<endl;
+			}
+		}
+		for(i=0; i<n; i++){
+			switch(datos[i]){
+			case 1:
+				cout<<"Introduzca un nuevo nombre:"<<endl;
+				cin>>cadena;
+				Get_Alumno(h).Set_Nombre(cadena);
+			break;
+			
+			case 2:
+				cout<<"Introduzca unos nuevos apellidos"<<endl;
+				cin>>cadena;
+				Get_Alumno(h).Set_Apellido(cadena);
+			break;
+			
+			case 3:
+				cout<<"Introduzca un nuevo DNI"<<endl;
+				cin>>cadena;		
+				Get_Alumno(h).Set_DNI(cadena);	
+			break;
+			
+			case 4:
+				cout<<"Introduzca un nuevo email"<<endl;
+				cin>>cadena;		
+				Get_Alumno(h).Set_Email(cadena);	
+			break;
+			
+			case 5:
+				cout<<"Introduzca una nueva direccion"<<endl;
+				cin>>cadena;	
+				Get_Alumno(h).Set_Direccion(cadena);		
+			break;
+			
+			case 6:
+				cout<<"Introduzca un nuevo telefono"<<endl;
+				cin>>aux;	
+				Get_Alumno(h).Set_Telefono(aux);		
+			break;
+			
+			case 7:
+				cout<<"Introduzca una nueva fecha de nacimiento"<<endl;
+				cin>>cadena;	
+				Get_Alumno(h).Set_Fecha_Nacimiento(cadena);		
+			break;
+			
+			case 8:
+				cout<<"Introduzca un nuevo curso mas alto"<<endl;
+				cin>>aux;	
+				Get_Alumno(h).Set_Curso(aux);		
+			break;
+			
+			case 9:
+				cout<<"Introduzca un nuevo equipo"<<endl;
+				cin>>aux;	
+				Get_Alumno(h).Set_Equipo(aux);		
+			break;
+			
+			case 10:
+				cout<<"Introduzca un nuevo Lider"<<endl;
+				cin>>lider;		
+				Get_Alumno(h).Set_Lider(lider);	
+			break;
+			
+			};	
+		}
+		cout<<"Cambios realizados con exito"<<endl;
+	}
+}
+
+void Profesor::Guardar_Fichero(){
+	string fichero;
+	cout<<"Introduzca el nombre del fichero: "<<endl;
+	cin>>fichero;
+}
+
+void Profesor::Cargar_Fichero(){
+	string fichero;
+	cout<<"Introduzca el nombre del fichero: "<<endl;
+	cin>>fichero;
+}
+
